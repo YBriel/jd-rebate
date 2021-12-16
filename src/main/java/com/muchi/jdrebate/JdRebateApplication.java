9b8d43b332d93176a1c40457a7681866f85838ed
yuzq <1065265077@qq.com>
2021-12-16 10:24:25 +0800

    Initial commit

---
+++ src/main/java/com/muchi/jdrebate/JdRebateApplication.java
@@ -0,0 +1,13 @@
+package com.muchi.jdrebate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdRebateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdRebateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muchi/jdrebate/JdRebateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muchi.jdrebate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdRebateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
